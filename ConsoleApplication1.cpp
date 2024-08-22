29871887b9a5e3be50e984ec21986b59391c3e8b
joaopedro-pavani <99080735+folivorasloth@users.noreply.github.com>
2024-08-22 18:02:19 -0300

    att

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,26 +4,26 @@ int main() {
 
 	float m1, m2, c, t1, t2, tempQ, tempF, tempFin, tot1, tot2;
 
-	std::cout << "temp aQ: ";
+	std::cout << "Insira a temperatura da agua quente: ";
 	std::cin >> tempQ;
-	
-	std::cout << "L aF: ";
+
+	std::cout << "\nLitros de agua fria para esquentar: ";
 	std::cin >> m2;
-	std::cout << "temp aF: ";
+	std::cout << "\nTemperatura da agua a ser esquentada: ";
 	std::cin >> tempF;
 
-	std::cout << "temp final: ";
+	std::cout << "\nTemperatura final desejada: ";
 	std::cin >> tempFin;
 
 	t1 = tempQ - tempFin;
 	t2 = tempFin - tempF;
 
-	
+
 	tot2 = m2 * t2;
 
 	m1 = tot2 / t1;
 
-	std::cout << "res: " << m1;
+	std::cout << "\n" << m1 << " litros de agua quente para esquentar " << m2 << " litros\n\n";
 
 	return 0;
 }
